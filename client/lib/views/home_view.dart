c9499a4c0d0c3b2cd66ade0deb659d823919f100
Bedoui <firas-bedoui@hotmail.fr>
2022-05-20 14:02:28 +0200

    Create HomeView which redirect To Signup and Signin

--- client/lib/views/home_view.dart
+++ client/lib/views/home_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'auth/signin_view.dart';
+import 'auth/signup_view.dart';
+
 class HomeView extends StatelessWidget {
   static const String routeName = '/';
   const HomeView({Key? key}) : super(key: key);
@@ -8,13 +11,53 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: EdgeInsets.all(20),
         alignment: Alignment.center,
-        child: const Text(
-          'HomeView ! ',
-          style: TextStyle(
-            fontSize: 35,
-            fontWeight: FontWeight.bold,
-          ),
+        color: Colors.black,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const Text(
+              'Bienvenu sur Dinya ! ',
+              style: TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 15),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pushNamed(context, SignupView.routeName);
+              },
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.grey.shade900,
+              ),
+              child: const Text(
+                'S\'inscrire',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, SigninView.routeName);
+              },
+              child: const Text(
+                'Connexion',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
