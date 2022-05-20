dd3c3d82cbe26212a806424a0951690c8ff19cd0
Bedoui <firas-bedoui@hotmail.fr>
2022-05-20 18:07:23 +0200

    Create View signin

--- client/lib/views/auth/signin_view.dart
+++ client/lib/views/auth/signin_view.dart
@@ -1,19 +1,118 @@
 import 'package:flutter/material.dart';
+import 'package:client/models/signin_form_model.dart';
 
-class SigninView extends StatelessWidget {
+class SigninView extends StatefulWidget {
   static const String routeName = '/signin';
   const SigninView({Key? key}) : super(key: key);
+
+  @override
+  State<SigninView> createState() => _SigninViewState();
+}
+
+class _SigninViewState extends State<SigninView> {
+  final GlobalKey<FormState> key = GlobalKey<FormState>();
+  late SigninForm signupForm;
+  FormState get form => key.currentState!;
+
+  @override
+  void initState() {
+    signupForm = SigninForm(email: null, password: null);
+    super.initState();
+  }
+
+  Future<void> submitForm() async {
+    if (form.validate()) {
+      form.save();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: const Text(
-          'SigninView ! ',
-          style: TextStyle(
-            fontSize: 35,
-            fontWeight: FontWeight.bold,
-          ),
+        padding:
+            const EdgeInsets.only(top: 40, bottom: 20, left: 20, right: 20),
+        color: Colors.black,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const Text(
+              'Connexion',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 15),
+            ),
+            Form(
+              key: key,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  const Text(
+                    'email',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 3),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey.shade900,
+                      filled: true,
+                    ),
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                    onSaved: ((newValue) => signupForm.email = newValue),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                  ),
+                  const Text(
+                    'password',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 3),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey.shade900,
+                      filled: true,
+                    ),
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                    onSaved: ((newValue) => signupForm.password = newValue),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                  ),
+                  ElevatedButton(
+                    onPressed: submitForm,
+                    child: const Text(
+                      'Se connecter',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
