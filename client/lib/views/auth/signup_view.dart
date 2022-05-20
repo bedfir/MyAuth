a9e5500383013324b8af7e8b0146ce0b93f58e31
Bedoui <firas-bedoui@hotmail.fr>
2022-05-20 14:05:02 +0200

    Create stf SignInView within Form to check if UserIsExist

--- client/lib/views/auth/signup_view.dart
+++ client/lib/views/auth/signup_view.dart
@@ -1,19 +1,151 @@
+import 'package:client/models/signup_form_model.dart';
+import 'package:client/views/auth/signin_view.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class SignupView extends StatelessWidget {
+import '../../providers/auth_provider.dart';
+
+class SignupView extends StatefulWidget {
   static const String routeName = '/signup';
   const SignupView({Key? key}) : super(key: key);
+
+  @override
+  State<SignupView> createState() => _SignupViewState();
+}
+
+class _SignupViewState extends State<SignupView> {
+  final GlobalKey<FormState> key = GlobalKey<FormState>();
+  late SignupForm signupForm;
+  FormState get form => key.currentState!;
+
+  @override
+  void initState() {
+    signupForm = SignupForm(email: null, username: null, password: null);
+    super.initState();
+  }
+
+  Future<void> submitForm() async {
+    if (form.validate()) {
+      form.save();
+      final error = await Provider.of<AuthProvider>(context, listen: false)
+          .signup(signupForm);
+
+      if (error == null) {
+        Navigator.pushNamed(context, SigninView.routeName);
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: const Text(
-          'SignupView ! ',
-          style: TextStyle(
-            fontSize: 35,
-            fontWeight: FontWeight.bold,
-          ),
+        padding:
+            const EdgeInsets.only(top: 40, bottom: 20, left: 20, right: 20),
+        color: Colors.black,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const Text(
+              'Inscription',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 15),
+            ),
+            Form(
+              key: key,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  const Text(
+                    'email',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 3),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey.shade900,
+                      filled: true,
+                    ),
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                    onSaved: ((newValue) => signupForm.email = newValue),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                  ),
+                  const Text(
+                    'username',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 3),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey.shade900,
+                      filled: true,
+                    ),
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                    onSaved: ((newValue) => signupForm.username = newValue),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                  ),
+                  const Text(
+                    'password',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 3),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey.shade900,
+                      filled: true,
+                    ),
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                    onSaved: ((newValue) => signupForm.password = newValue),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                  ),
+                  ElevatedButton(
+                    onPressed: submitForm,
+                    child: const Text(
+                      'S\'inscrire',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
