d367e0f02633b7ab2c1e04b6fe51bfc9453722e6
Bedoui <firas-bedoui@hotmail.fr>
2022-05-20 13:59:03 +0200

    add MultiProvider

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:client/providers/auth_provider.dart';
 import 'package:client/views/auth/signin_view.dart';
 import 'package:client/views/auth/signup_view.dart';
 import 'package:client/views/not_found_view.dart';
 import 'package:client/views/profile_view.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import './views/home_view.dart';
 
 void main() {
@@ -17,28 +19,36 @@ class MyAuth extends StatefulWidget {
 }
 
 class _MyAuthState extends State<MyAuth> {
+  final AuthProvider authProvider = AuthProvider();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'my auth',
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      home: HomeView(),
-      onGenerateRoute: (RouteSettings settings) {
-        switch (settings.name) {
-          case SigninView.routeName:
-            MaterialPageRoute(builder: (_) => SigninView());
-            break;
-          case SignupView.routeName:
-            MaterialPageRoute(builder: (_) => SignupView());
-            break;
-          case ProfileView.routeName:
-            MaterialPageRoute(builder: (_) => ProfileView());
-            break;
-        }
-      },
-      onUnknownRoute: (settings) {
-        MaterialPageRoute(builder: (_) => NotFoundView());
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: authProvider,
+        ),
+      ],
+      child: MaterialApp(
+        title: 'my auth',
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        home: HomeView(),
+        onGenerateRoute: (settings) {
+          if (settings.name == HomeView.routeName) {
+            return MaterialPageRoute(builder: (_) => const HomeView());
+          } else if (settings.name == SigninView.routeName) {
+            return MaterialPageRoute(builder: (_) => const SigninView());
+          } else if (settings.name == SignupView.routeName) {
+            return MaterialPageRoute(builder: (_) => const SignupView());
+          } else if (settings.name == ProfileView.routeName) {
+            return MaterialPageRoute(builder: (_) => const ProfileView());
+          } else {
+            return null;
+          }
+        },
+        onUnknownRoute: (settings) {
+          MaterialPageRoute(builder: (_) => NotFoundView());
+        },
+      ),
     );
   }
 }
