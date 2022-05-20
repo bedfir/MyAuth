c7c0ddc860f3cbb0b73ee01e6e44a5dfcea50e20
Bedoui <firas-bedoui@hotmail.fr>
2022-05-21 01:28:39 +0200

    add signin methode to call DB return User AND Token

--- client/lib/providers/auth_provider.dart
+++ client/lib/providers/auth_provider.dart
@@ -1,11 +1,15 @@
 import 'package:client/models/signup_form_model.dart';
+import 'package:client/models/user_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import '../models/signin_form_model.dart';
+
 class AuthProvider with ChangeNotifier {
   final String host = 'http://10.0.2.2';
   bool isLoading = false;
+  late String? token;
 
   Future<dynamic> signup(SignupForm signupForm) async {
     try {
@@ -20,6 +24,30 @@ class AuthProvider with ChangeNotifier {
         return json.decode(response.body);
       }
       return null;
+    } catch (e) {
+      isLoading = false;
+      rethrow;
+    }
+  }
+
+  Future<dynamic> sigin(SigninForm signinForm) async {
+    try {
+      isLoading = true;
+      http.Response response = await http.post(
+        Uri.parse('$host/api/auth'),
+        headers: {'Content-type': 'application/json'},
+        body: json.encode(signinForm.toJson()),
+      );
+
+      final Map<String, dynamic> body = json.decode(response.body);
+
+      if (response.statusCode == 200) {
+        final User user = User.fromJson(body['user']);
+        token = body['token'];
+        return user;
+      } else {
+        return body;
+      }
     } catch (e) {
       isLoading = false;
       rethrow;
